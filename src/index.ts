0a860978ed2f39f7ba635ec5f06be966b4e47995
Elijah Mylcev <elijahmylcev@gmail.com>
2022-08-12 21:56:17 +0300

    Types in TS --> Tuples

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
-let age: number = 20;
-if (age < 10) {
-  age += 10;
+let sales: number = 123_456_789;
+let course: string = 'TypeScript';
+let is_published: boolean = true;
+let level; //any
+
+// The any type
+level = 1;  // Bad practice
+level = 'a';  // Bad practice
+
+function render(document: any) {
+  console.log(document);
 }
+
+// The array type
+
+let numbers: number[] = [1, 2, 3]
+let numbersAny: any[] = []
+
+numbers.forEach(n => n.toLocaleString)
+
+// Tuples = Картежи
