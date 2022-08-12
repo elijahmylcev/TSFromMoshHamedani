2b5de23fe62b00882db18d6b708cbbe0a8d401fd
Elijah Mylcev <elijahmylcev@gmail.com>
2022-08-12 22:33:08 +0300

    functions

--- src/index.ts
+++ src/index.ts
@@ -36,3 +36,14 @@ const large = 3;
 const enum Size {Small = 1, Medium, Large}
 let mySize: Size = Size.Medium;
 console.log(mySize);
+
+// functions
+function calculateTax(income: number, taxYear = 2022): number {
+ if ((taxYear || 2022) < 50000) {
+  return income * 1.2;
+ }
+ return income * 1.3
+}
+
+calculateTax(10_000)
+// void - пустота
